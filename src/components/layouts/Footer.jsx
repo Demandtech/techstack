d34ec6f35b368806fd936066688eadd7e068bc97
rasheed <demandwork247@gmail.com>
2024-12-20 05:59:12 +0100

    footer fixed

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,57 +1,56 @@
-import React from "react";
 import logo from "../../assets/logo-white.png";
 import Button from "../Button";
 
 function Footer() {
-	return (
-		<footer className="text-white">
-			<div className="max-w-[90%] pt-16 font-cousine lg:max-w-[90%] mx-auto">
-				<div>
-					<div
-						style={{ backgroundSize: "100% 100%" }}
-						className="bg-center bg-no-repeat bg-footersm md:bg-footer py-10 px-5 lg:px-16 flex flex-col justify-between gap-5 md:flex-row"
-					>
-						<div className="">
-							<p className="text-2xl md:text-4xl font-cousine">
-								Subscribe to get <br /> the latest updates
-							</p>
-						</div>
-						<form className="bg-white flex gap-2 items-center px-2 py-4 lg:px-4">
-							<input
-								placeholder="Enter Your Email"
-								className="placeholder:text-ellipsis w-full text-black focus:outline-none"
-							/>
-							<Button color="blue"> SUBSCRIBE</Button>
-						</form>
-					</div>
-					<div className="flex py-7 gap-3 flex-wrap items-center justify-center md:justify-between">
-						<div className="max-w-[200px] lg:max-w-[300px]">
-							<img src={logo} />
-						</div>
-						<ul className="flex flex-wrap gap-5 justify-center md:justify-normal">
-							<li>
-								<a href="">Discord</a>
-							</li>
-							<li>
-								<a href="">Facebook</a>
-							</li>
-							<li>
-								<a href="">Instagram</a>
-							</li>
-							<li>
-								<a href="">X (Twitter)</a>
-							</li>
-						</ul>
-					</div>
-				</div>
-				<div className="py-4 lg:py-3 text-center border-t border-grey">
-					<p className="text-xs lg:text-sm">
-						{new Date().getFullYear()} &copy; TechStack. All right reserved
-					</p>
-				</div>
-			</div>
-		</footer>
-	);
+  return (
+    <footer className="text-white">
+      <div className="max-w-[90%] pt-16 font-cousine lg:max-w-[90%] mx-auto">
+        <div>
+          <div
+            style={{ backgroundSize: "100% 100%" }}
+            className="bg-center bg-no-repeat bg-footersm md:bg-footer py-10 px-5 lg:px-16 flex flex-col justify-between gap-5 md:flex-row"
+          >
+            <div className="">
+              <p className="text-2xl md:text-4xl font-cousine">
+                Subscribe to get <br /> the latest updates
+              </p>
+            </div>
+            <form className="bg-white flex gap-2 items-center px-2 py-4 lg:px-4">
+              <input
+                placeholder="Enter Your Email"
+                className="placeholder:text-ellipsis w-full text-black focus:outline-none"
+              />
+              <Button color="blue"> SUBSCRIBE</Button>
+            </form>
+          </div>
+          <div className="flex py-7 gap-3 flex-wrap items-center justify-center md:justify-between">
+            <div className="max-w-[200px] lg:max-w-[300px]">
+              <img src={logo} />
+            </div>
+            <ul className="flex flex-wrap gap-5 justify-center md:justify-normal">
+              <li className="hover:opacity-50 transition-opacity duration-300">
+                <a href="">Discord</a>
+              </li>
+              <li className="hover:opacity-50 transition-opacity duration-300">
+                <a href="">Facebook</a>
+              </li>
+              <li className="hover:opacity-50 transition-opacity duration-300">
+                <a href="">Instagram</a>
+              </li>
+              <li className="hover:opacity-50 transition-opacity duration-300">
+                <a href="">X (Twitter)</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="py-4 lg:py-3 text-center border-t border-grey">
+          <p className="text-xs lg:text-sm">
+            {new Date().getFullYear()} &copy; TechStack. All right reserved
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -6,145 +6,145 @@ import { MenuBar, CloseIcon } from "../Svgs";
 import { NavLink, Link } from "react-router-dom";
 
 export default function Header() {
-	const [openMenu, setOpenMenu] = useState(false);
-	const [isSticky, setIsSticky] = useState(false);
-	const [scrollPos, setScrollPos] = useState(0);
-	const headerRef = useRef(null);
-	const isInView = useInView(headerRef);
+  const [openMenu, setOpenMenu] = useState(false);
+  const [isSticky, setIsSticky] = useState(false);
+  const [scrollPos, setScrollPos] = useState(0);
+  const headerRef = useRef(null);
+  const isInView = useInView(headerRef);
 
-	const headerVariants = {
-		hidden: {
-			opacity: 0,
-			y: -40,
-		},
-		visible: {
-			opacity: 1,
-			y: 0,
+  const headerVariants = {
+    hidden: {
+      opacity: 0,
+      y: -40,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
 
-			transition: { duration: 0.005 },
-		},
-	};
+      transition: { duration: 0.005 },
+    },
+  };
 
-	const handleScroll = useCallback(() => {
-		if (openMenu) return;
-		const currentScrollPos = window.scrollY;
+  const handleScroll = useCallback(() => {
+    if (openMenu) return;
+    const currentScrollPos = window.scrollY;
 
-		if (currentScrollPos > 400 && currentScrollPos < scrollPos) {
-			setIsSticky(true);
-		} else {
-			setIsSticky(false);
-		}
+    if (currentScrollPos > 400 && currentScrollPos < scrollPos) {
+      setIsSticky(true);
+    } else {
+      setIsSticky(false);
+    }
 
-		setScrollPos(currentScrollPos);
-	}, [openMenu, scrollPos]);
+    setScrollPos(currentScrollPos);
+  }, [openMenu, scrollPos]);
 
-	useEffect(() => {
-		window.addEventListener("scroll", handleScroll);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
 
-		return () => window.removeEventListener("scroll", handleScroll);
-	}, [scrollPos, openMenu]);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [scrollPos, openMenu]);
 
-	useEffect(() => {
-		const handleOutsideClick = (e) => {
-			if (headerRef.current && !headerRef.current.contains(e.target)) {
-				setOpenMenu(false);
-			}
-		};
+  useEffect(() => {
+    const handleOutsideClick = (e) => {
+      if (headerRef.current && !headerRef.current.contains(e.target)) {
+        setOpenMenu(false);
+      }
+    };
 
-		if (openMenu) {
-			document.body.style.overflow = "hidden";
-			document.addEventListener("mousedown", handleOutsideClick);
-			document.addEventListener("touchstart", handleOutsideClick);
-		} else {
-			document.body.style.overflow = "";
-		}
+    if (openMenu) {
+      document.body.style.overflow = "hidden";
+      document.addEventListener("mousedown", handleOutsideClick);
+      document.addEventListener("touchstart", handleOutsideClick);
+    } else {
+      document.body.style.overflow = "";
+    }
 
-		return () => {
-			document.body.style.overflow = "";
-		};
-	}, [openMenu]);
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [openMenu]);
 
-	return (
-		<motion.header
-			initial="hidden"
-			variants={headerVariants}
-			animate={isInView ? "visible" : "hidden"}
-			ref={headerRef}
-			id="header"
-			className={`${
-				isSticky ? "sticky z-50" : "absolute"
-			} backdrop-blur-lg bg-transparent pt-5 md:pt-10 pb-5 top-0 text-white w-full transition-all duration-300 ease-linear`}
-		>
-			<div
-				style={{
-					backgroundSize: "100% 100%",
-				}}
-				className={`relative md:bg-header md:py-3  max-w-[95%] md:max-w-[90%] mx-auto`}
-			>
-				<div className="mx-auto flex flex-col md:flex-row md:items-center justify-between md:gap-5 md:px-10">
-					<div
-						style={{
-							backgroundSize: "100% 100%",
-						}}
-						className={`py-5  px-10 md:px-0 md:py-0 flex justify-between items-center bg-headersm md:bg-none`}
-					>
-						<Link to="/">
-							<img className="w-[150px]" src={logo} />
-						</Link>
-						<button
-							onClick={() => setOpenMenu((prev) => !prev)}
-							className="md:hidden"
-						>
-							{openMenu ? <CloseIcon /> : <MenuBar />}
-						</button>
-					</div>
-					{/* Mobile Nav */}
-					<motion.nav
-						initial={{ height: 0 }}
-						animate={{ height: openMenu ? 250 : 0 }}
-						className={`overflow-hidden md:px-0 bg-light-blue`}
-					>
-						<ul className="md:hidden border-t-4 px-2 md:border-b-0 border-black font-chivo py-5 lg:py-0 text-black flex flex-col gap-8">
-							<li onClick={() => setOpenMenu(false)}>
-								<Link to="/#speakers">SPEAKERS</Link>
-							</li>
-							<li onClick={() => setOpenMenu(false)}>
-								<Link to="/#sponsors">SPONSORS</Link>
-							</li>
-							<li onClick={() => setOpenMenu(false)}>
-								<NavLink className="link" to="/contact">
-									CONTACT
-								</NavLink>
-							</li>
-							<li className="cursor-pointer max-w-[170px]">
-								<Button aria-label="ticket button" color="black">
-									GET TICKETS
-								</Button>
-							</li>
-						</ul>
-					</motion.nav>
-					<nav>
-						<ul className="hidden md:flex px-2 md:border-b-0 border-black font-chivo py-5 lg:py-0 text-black  items-center flex-row gap-8">
-							<li onClick={() => setOpenMenu(false)}>
-								<Link to="/#speakers">SPEAKERS</Link>
-							</li>
-							<li onClick={() => setOpenMenu(false)}>
-								<Link to="/#sponsors">SPONSORS</Link>
-							</li>
-							<li onClick={() => setOpenMenu(false)}>
-								<NavLink className="link" to="/contact">
-									CONTACT
-								</NavLink>
-							</li>
-							<li className="cursor-pointer">
-								<Button aria-label="ticket button" color="black">
-									GET TICKETS
-								</Button>
-							</li>
-						</ul>
-					</nav>
-				</div>
-			</div>
-		</motion.header>
-	);
+  return (
+    <motion.header
+      initial="hidden"
+      variants={headerVariants}
+      animate={isInView ? "visible" : "hidden"}
+      ref={headerRef}
+      id="header"
+      className={`${
+        isSticky ? "sticky z-50" : "absolute"
+      } backdrop-blur-lg bg-transparent pt-5 md:pt-10 pb-5 top-0 text-white w-full transition-all duration-300 ease-linear`}
+    >
+      <div
+        style={{
+          backgroundSize: "100% 100%",
+        }}
+        className={`relative md:bg-header md:py-3  max-w-[95%] md:max-w-[90%] mx-auto`}
+      >
+        <div className="mx-auto flex flex-col md:flex-row md:items-center justify-between md:gap-5 md:px-10">
+          <div
+            style={{
+              backgroundSize: "100% 100%",
+            }}
+            className={`py-5  px-10 md:px-0 md:py-0 flex justify-between items-center bg-headersm md:bg-none`}
+          >
+            <Link to="/">
+              <img className="w-[150px]" src={logo} />
+            </Link>
+            <button
+              onClick={() => setOpenMenu((prev) => !prev)}
+              className="md:hidden"
+            >
+              {openMenu ? <CloseIcon /> : <MenuBar />}
+            </button>
+          </div>
+          {/* Mobile Nav */}
+          <motion.nav
+            initial={{ height: 0 }}
+            animate={{ height: openMenu ? 250 : 0 }}
+            className={`overflow-hidden md:px-0 bg-light-blue`}
+          >
+            <ul className="md:hidden border-t-4 px-2 md:border-b-0 border-black font-chivo py-5 lg:py-0 text-black flex flex-col gap-8">
+              <li onClick={() => setOpenMenu(false)}>
+                <Link to="/#speakers">SPEAKERS</Link>
+              </li>
+              <li onClick={() => setOpenMenu(false)}>
+                <Link to="/#sponsors">SPONSORS</Link>
+              </li>
+              <li onClick={() => setOpenMenu(false)}>
+                <NavLink className="link" to="/contact">
+                  CONTACT
+                </NavLink>
+              </li>
+              <li className="cursor-pointer max-w-[170px]">
+                <Button aria-label="ticket button" color="black">
+                  GET TICKETS
+                </Button>
+              </li>
+            </ul>
+          </motion.nav>
+          <nav>
+            <ul className="hidden md:flex px-2 md:border-b-0 border-black font-chivo py-5 lg:py-0 text-black  items-center flex-row gap-8">
+              <li onClick={() => setOpenMenu(false)}>
+                <Link to="/#speakers">SPEAKERS</Link>
+              </li>
+              <li onClick={() => setOpenMenu(false)}>
+                <Link to="/#sponsors">SPONSORS</Link>
+              </li>
+              <li onClick={() => setOpenMenu(false)}>
+                <NavLink className="link" to="/contact">
+                  CONTACT
+                </NavLink>
+              </li>
+              <li className="cursor-pointer">
+                <Button aria-label="ticket button" color="black">
+                  GET TICKETS
+                </Button>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </motion.header>
+  );
 }
